aed9fad077e7cec9551ce3a3991a95cecb677292
ishwordeep <ishwor055@gmail.com>
2024-11-14 14:10:09 +0545

    api

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,5 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
-    protected $fillable = [];
+    protected $fillable = [
+        "name","agent_link","player_link","image","display_order"
+    ];
 }
